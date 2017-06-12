45673a4e298e62490212f63436aef51340c7e313
Dan Siberry <dansiberry1@gmail.com>
2017-06-12 11:43:14 +0200

    Update 2017-06-12T09:43:11.363Z

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function openMenu() {
-  var menu = document.getElementById('mobile-menu-section')
-    if (menu.style.height === '0px') {
-        menu.style.height = "200px";
-    } else {
-        menu.style.height = "0px";
-    }
-};
